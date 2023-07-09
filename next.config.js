1c89b3c85c1e12f85873d0e12a4a83a56c8c771c
Daniel B. Lapinig III <dltechy@gmail.com>
2023-07-10 02:30:19 +0800

    Ignore lint & typescript checks on build

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
